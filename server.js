53d30a82c80cc393b5afe4812813e0b57fbde13e
Jimmy Longley <jimmylongley@gmail.com>
2018-01-18 17:03:45 -0800

    setting up basic express app with GET AND POST

--- server.js
+++ server.js
@@ -1,9 +1,66 @@
-const express = require("express");
+'use strict'
 
-const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compression = require('compression')
+const express = require('express')
+const morgan = require('morgan')
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+const PORT = process.env.PORT || 3001
 
-app.listen(3001);
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+
+app.use(compression())
+app.use(morgan('common'))
+
+let todos = [{
+  id: 123,
+  value: 'one two three',
+  done: false
+},
+{
+  id: 122,
+  value: 'one two three',
+  done: false
+}]
+let id = todos.reduce((maxId, todo) => {
+  return Math.max(maxId, todo.id)
+}, 0) + 1
+
+app.get('/', (req, res) => {
+  res.send('Use the API dummy')
+})
+
+app.get('/api/todos/', (req, res) => {
+  res.send(todos)
+})
+
+app.post('/api/todos/', (req, res) => {
+  if (!req.body.done || !req.body.value) {
+    const error = {
+      status: 400,
+      message: `Missing 'done' or 'value' parameter`
+    }
+    console.error(`ERROR: ${error.message}`)
+    return res.status(500).send(error)
+  }
+
+  const newTodo = {
+    id: id++,
+    value: req.body.value,
+    done: req.body.done
+  }
+  todos.push(newTodo)
+
+  console.log('Added new todo', newTodo)
+  res.send(newTodo)
+})
+
+app.listen(3001, () => { console.log(`Listening on port ${PORT}`) })
