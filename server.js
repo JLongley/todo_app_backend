74ffc39006e38db5ac91fe690f80ac5a27cf202d
Jimmy Longley <jimmylongley@gmail.com>
2018-01-18 17:57:48 -0800

    adding update and delete

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict'
 
+const _ = require('lodash')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const compression = require('compression')
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
@@ -30,6 +31,7 @@ let todos = [{
   value: 'one two three',
   done: false
 }]
+
 let id = todos.reduce((maxId, todo) => {
   return Math.max(maxId, todo.id)
 }, 0) + 1
@@ -49,7 +51,7 @@ app.post('/api/todos/', (req, res) => {
       message: `Missing 'done' or 'value' parameter`
     }
     console.error(`ERROR: ${error.message}`)
-    return res.status(500).send(error)
+    return res.status(400).send(error)
   }
 
   const newTodo = {
@@ -60,7 +62,37 @@ app.post('/api/todos/', (req, res) => {
   todos.push(newTodo)
 
   console.log('Added new todo', newTodo)
-  res.send(newTodo)
+  res.status(201).send(newTodo)
+})
+
+app.put('/api/todos/:id', (req, res) => {
+  if (!req.body.done || !req.body.value || !req.body.id) {
+    const error = {
+      status: 400,
+      message: `Missing 'done', 'id', or 'value' parameter`
+    }
+    console.error(`ERROR: ${error.message}`)
+    return res.status(400).send(error)
+  }
+  const todo = _.find(todos, (todo) => todo.id == req.params.id)
+  console.log(todos, todo)
+  if (!todo) {
+    const error = {
+      status: 404,
+      message: `todo ${req.params.id} not found`
+    }
+    console.error(`ERROR: ${error.message}`)
+    return res.status(404).send(error)
+  }
+  todo.value = req.body.value
+  todo.done = req.body.done
+
+  res.send(todo)
+})
+
+app.delete('/api/todos/:id', (req, res) => {
+  todos = todos.filter((todo) => todo.id != req.params.id)
+  res.status(204).send()
 })
 
 app.listen(3001, () => { console.log(`Listening on port ${PORT}`) })
